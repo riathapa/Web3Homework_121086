787b945773ec687724dd8b4d41b1437ea5fa9358
mikasa <69318274+riathapa@users.noreply.github.com>
2024-11-06 18:01:11 -0500

    Update Interact.py

--- src/Interact.py
+++ src/Interact.py
@@ -4,14 +4,14 @@ import os
 
 
 contract = "SimpleStorage.sol"
-account = os.getenv("ACCOUNT")
-private_key = os.getenv("PRIVATE_KEY")
-provider = os.getenv("LOCAL_PROVIDER")
+ACCOUNT = os.getenv("ACCOUNT")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+PROVIDER = os.getenv("LOCAL_PROVIDER")
 
-chain_id = int(os.getenv("CHAIN_ID"))
+chain_id = 31337
     
-connection = Web3(Web3.HTTPProvider(provider))
-contract_address, abi = deploy_contract(contract, "SimpleStorage", account, private_key, provider, chain_id)
+connection = Web3(Web3.HTTPProvider(PROVIDER))
+contract_address, abi = deploy_contract(contract, "SimpleStorage", ACCOUNT, PRIVATE_KEY, PROVIDER, chain_id)
 
 # contract_address, abi = deploy_contract("SimpleStorage.sol", "SimpleStorage", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "HTTP://127.0.0.1:8545", 31337)
 
@@ -27,7 +27,7 @@ transaction = simple_storage.functions.set(150005647).build_transaction(
     }
 )
 
-signed_txn = connection.eth.account.sign_transaction(transaction, private_key = private_key)
+signed_txn = connection.eth.account.sign_transaction(transaction, private_key = PRIVATE_KEY)
 print("Updated sotred value ")
 tx_hash = connection.eth.send_raw_transaction(signed_txn.raw_transaction)
 
