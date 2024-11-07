395c8d5cc78e0f388f23f9690328bbf46bac06e1
mikasa <69318274+riathapa@users.noreply.github.com>
2024-11-06 20:01:33 -0500

    Update Interact.py

--- src/Interact.py
+++ src/Interact.py
@@ -8,7 +8,7 @@ ACCOUNT = os.getenv("ACCOUNT")
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 PROVIDER = os.getenv("LOCAL_PROVIDER")
 
-chain_id = 31337
+chain_id = int(31337)
     
 connection = Web3(Web3.HTTPProvider(PROVIDER))
 contract_address, abi = deploy_contract(contract, "SimpleStorage", ACCOUNT, PRIVATE_KEY, PROVIDER, chain_id)
@@ -16,13 +16,13 @@ contract_address, abi = deploy_contract(contract, "SimpleStorage", ACCOUNT, PRIV
 # contract_address, abi = deploy_contract("SimpleStorage.sol", "SimpleStorage", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "HTTP://127.0.0.1:8545", 31337)
 
 simple_storage = connection.eth.contract(address=contract_address, abi = abi)
-nonce = connection.eth.get_transaction_count(account)
+nonce = connection.eth.get_transaction_count(ACCOUNT)
 
 transaction = simple_storage.functions.set(150005647).build_transaction(
     {
             "chainId":chain_id,
             "gasPrice":connection.eth.gas_price,
-            "from":account,
+            "from":ACCOUNT,
             "nonce":nonce
     }
 )
